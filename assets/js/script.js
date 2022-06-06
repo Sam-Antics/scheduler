6a486f1e91f93a6cae3559e19a19705b682a00d8
SEGuerra <sameguerra@sbcglobal.net>
2022-06-05 19:22:23 -0500

    HTML, CSS, and JS set-up

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+/****************************/
+/***PSEUDOCODE FOR PROJECT***/
+/***************************/
+
+// When the planner is opened, the current day displays at the top of the page
+var currentDay = moment().format("dddd LL");
+$("#currentDay").append(currentDay);
+
+// set up time blocks for standard business hours
+
+// each time block is color-coded to indicate past, present, or future
+
+// clicking into the time block opens textarea
+
+// save button for each time block
+
+// save to localStorage for persistent data
+
